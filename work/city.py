4f026ce5ab70974096a4277956cc24d24e8596cb
podv2 <Tapakahhhh12@gmail.com>
2026-05-07 10:41:47 +0300

    python first

---
+++ work/city.py
@@ -0,0 +1,8 @@
+city = "Санкт-Петербург"
+year = 2026
+is_learning = True
+print(city, year, is_learning)
+print(city)
+print(year)
+print(is_learning)
+print(type(year))
---
+++ work/lesson2.py
@@ -0,0 +1,4 @@
+name = input("Как тебя зовут? ")
+year = int(input("Какой у тебя год рождения? "))
+age = 2026 - year
+print(f"Привет, {name}! Тебе {age} лет.")
